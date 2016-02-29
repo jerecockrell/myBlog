1b4642ea005f8d56ae7b30c835bab435125f4d4b
jerecockrell <jere.cockrell@gmail.com>
2016-02-29 14:09:27 -0700

    first commit on new branch

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var passport = require('passport');
+var flash = require('connect-flash');
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/myblog');
@@ -10,7 +13,31 @@ var postRoutes = require('./routes/posts')
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(session({
+ secret: 'ilovescotchscotchyscotchscotch'
+})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(session({
+ cookie: {
+   maxAge: 60000
+ }
+}));
+app.use(flash());
+
+require('./config/passport')(passport);
+// routes ======================================================================
+require('./routes/user.js')(app, passport);
+
 var port = process.env.PORT || 8080;
+
+app.use(express.static('public'));
+
+app.use(function(req, res, next){
+  var user = req.user || "no user";
+  console.log(user);
+  next();
+});
 
 app.set('view engine', 'ejs')
 
@@ -38,6 +65,10 @@ app.get('/blog', function(req, res) {
 
 app.get('/aboutblog', function(req, res) {
 	res.render('aboutblog')
+})
+
+app.get('/contact', function(req, res) {
+	res.render('contact')
 })
 
 app.use('/api', postRoutes);
